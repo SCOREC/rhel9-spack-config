c791c8aa6d95a8e01573743adb193bdec6b93d33
Cameron Smith <smithc11@rpi.edu>
2023-10-21 14:06:26 -0400

    need gcc12 for cuda12

--- v0.20.1_4/initialSetup.sh
+++ v0.20.1_4/initialSetup.sh
@@ -4,20 +4,27 @@ spack add $coreGcc +binutils %gcc@11.3.1_rhel9
 spack concretize
 spack install --fail-fast
 spack compiler add $(spack location -i $coreGcc)
+
+spack add gcc@12.3.0 %${coreGcc}
+spack concretize
+spack install --fail-fast
+spack compiler add $(spack location -i gcc@12.3.0)
+spack add cuda@12.1.1 %gcc@12.3.0
+spack concretize
+spack install --fail-fast
+spack add mpich %gcc@12.3.0
+spack concretize
+spack install --fail-fast
+
 spack add gcc@13.1.0 %${coreGcc}
 spack concretize
 spack install --fail-fast
 spack compiler add $(spack location -i gcc@13.1.0)
+spack add mpich %gcc@13.1.0
+spack concretize
+spack install --fail-fast
+
 spack add llvm@16.0.2 +clang %${coreGcc}
 spack concretize
 spack install --fail-fast
 spack compiler add $(spack location -i llvm@16.0.2)
-spack add mpich %gcc@13.1.0
-spack concretize
-spack install --fail-fast
-spack add cuda %gcc@13.1.0
-spack concretize
-spack install --fail-fast
-spack add mpich %clang@16.0.2
-spack concretize
-spack install --fail-fast
